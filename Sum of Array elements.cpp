2a5789ee8bbcf10911ed82fe87cc73c6b58bf0c9
GARUDA393 <117139434+GARUDA393@users.noreply.github.com>
2023-12-03 18:10:55 +0530

    Create Sum of Array elements.cpp

---
+++ Sum of Array elements.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   int a[10] = {1,2,3,4,5,6,7,8,9,10};
+   int sum=0;
+   for(int i=0; i<=10; i++){
+       sum = (sum + a[i]);
+   }
+   cout<<"Sum of array elements: "<<sum;
+}
